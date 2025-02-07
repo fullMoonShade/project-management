c9073c3253cfd47b46d2333c900411c66ff9e44e
Shei <72736290+fullMoonShade@users.noreply.github.com>
2025-02-07 21:18:34 +0600

    bg

--- molecule/src/app/page.tsx
+++ molecule/src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
-import Navbar from "@/components/navbar/navbar";
+import Aurora from '@/jsrepo/Backgrounds/Aurora/Aurora';
+// import Navbar from "@/components/navbar/navbar"; //Improve this
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      <Aurora
+      colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+      speed={0.5}
+      />
     </div>
   );
 }
