ea89f0f55648b02f91deecd7071beeef1843a914
Shei <72736290+fullMoonShade@users.noreply.github.com>
2025-02-08 11:37:01 +0600

    Added MenuBar

--- molecule/src/app/page.tsx
+++ molecule/src/app/page.tsx
@@ -2,11 +2,12 @@
 
 import Image from "next/image";
 import Orb from "@/jsrepo/Backgrounds/Orb/Orb";
-// import Navbar from "@/components/navbar/navbar"; //Improve this
+import { MenuBar } from "@/components/navbar/MenuBar"; 
 
 export default function Home() {
   return (
     <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+      <MenuBar />
     <Orb
       hoverIntensity={0.2 }
       rotateOnHover={true}
